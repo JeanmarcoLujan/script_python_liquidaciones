4d96793d72a3a9ea34e295cf7fd66bd3978b4101
JeanmarcoLujan <mark.15@hotmail.es>
2023-09-19 00:01:55 -0500

    soportar cualquier version de excel, manejo de excepciones, v3

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import xml.etree.ElementTree as ET
-from openpyxl import Workbook
+#from openpyxl import Workbook
 from colorama import init, Fore, Style
+import xlsxwriter
 
 # Inicializa colorama
 init(autoreset=True)
@@ -18,91 +19,124 @@ def generate_result():
     namespace = {"cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"}
     namespace1 = {"cac": "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"}
 
+    try:
 
-    # Recorremos los archivos XML en el directorio
-    for filename in os.listdir(xml_directory):
-        if filename.endswith(".xml"):
-            filepath = os.path.join(xml_directory, filename)
+        # Recorremos los archivos XML en el directorio
+        for filename in os.listdir(xml_directory):
+            if filename.endswith(".xml"):
+                filepath = os.path.join(xml_directory, filename)
+                
+                tree = ET.parse(filepath)
+                root = tree.getroot()
+
+                # Obtener atributos
+                invoice_id = root.find(".//cbc:ID", namespaces=namespace).text
+                issue_date = root.find(".//cbc:IssueDate", namespaces=namespace).text
+                invoice_type_code = root.find(".//cbc:InvoiceTypeCode", namespaces=namespace).get("listID")
+                taxable_amount = root.find(".//cbc:TaxableAmount", namespaces=namespace).text
+                ruc_emisor = ""
+                doc_vendedor = ""
+                registration_name = ""
+                address_line = ""
+                amount_value = ""
+                CountrySubentity = ""
+                party_identification = root.find(".//cac:AccountingCustomerParty/cac:Party/cac:PartyIdentification", namespaces=namespace1)
+                if party_identification is not None:
+                    ruc_emisor = party_identification.find(".//cbc:ID", namespaces=namespace).text
+                
+                party_identification1 = root.find(".//cac:AccountingSupplierParty/cac:Party/cac:PartyIdentification", namespaces=namespace1)
+                if party_identification1 is not None:
+                    doc_vendedor = party_identification1.find(".//cbc:ID", namespaces=namespace).text
+
+                party_legal_entity = root.find(".//cac:AccountingSupplierParty/cac:Party/cac:PartyLegalEntity", namespaces=namespace1)
+                if party_legal_entity is not None:
+                    registration_name = party_legal_entity.find(".//cbc:RegistrationName", namespaces=namespace).text
+
+                party_legal_entity1 = root.find(".//cac:AccountingSupplierParty/cac:Party/cac:PartyLegalEntity/cac:RegistrationAddress", namespaces=namespace1)
+                if party_legal_entity1 is not None:
+                    CountrySubentity = party_legal_entity1.find(".//cbc:Line", namespaces=namespace).text +" "+ party_legal_entity1.find(".//cbc:CountrySubentity", namespaces=namespace).text + "-" + party_legal_entity1.find(".//cbc:CityName", namespaces=namespace).text + "-" + party_legal_entity1.find(".//cbc:District", namespaces=namespace).text
+
+                registration_address = root.find(".//cac:DeliveryTerms/cac:DeliveryLocation/cac:Address", namespaces=namespace1)
+                if registration_address is not None:
+                    address_line = registration_address.find(".//cbc:Line", namespaces=namespace).text
             
-            tree = ET.parse(filepath)
-            root = tree.getroot()
+                tax_inclusive_amount = root.find(".//cac:LegalMonetaryTotal", namespaces=namespace1)
+                if tax_inclusive_amount is not None:
+                    amount_value = tax_inclusive_amount.find(".//cbc:TaxInclusiveAmount", namespaces=namespace).text
 
-            # Obtener atributos
-            invoice_id = root.find(".//cbc:ID", namespaces=namespace).text
-            issue_date = root.find(".//cbc:IssueDate", namespaces=namespace).text
-            invoice_type_code = root.find(".//cbc:InvoiceTypeCode", namespaces=namespace).get("listID")
-            taxable_amount = root.find(".//cbc:TaxableAmount", namespaces=namespace).text
-            ruc_emisor = ""
-            doc_vendedor = ""
-            registration_name = ""
-            address_line = ""
-            amount_value = ""
-            CountrySubentity = ""
-            party_identification = root.find(".//cac:AccountingCustomerParty/cac:Party/cac:PartyIdentification", namespaces=namespace1)
-            if party_identification is not None:
-                ruc_emisor = party_identification.find(".//cbc:ID", namespaces=namespace).text
-            
-            party_identification1 = root.find(".//cac:AccountingSupplierParty/cac:Party/cac:PartyIdentification", namespaces=namespace1)
-            if party_identification1 is not None:
-                doc_vendedor = party_identification1.find(".//cbc:ID", namespaces=namespace).text
 
-            party_legal_entity = root.find(".//cac:AccountingSupplierParty/cac:Party/cac:PartyLegalEntity", namespaces=namespace1)
-            if party_legal_entity is not None:
-                registration_name = party_legal_entity.find(".//cbc:RegistrationName", namespaces=namespace).text
+                atributos.append((ruc_emisor, invoice_id, issue_date, doc_vendedor, registration_name, CountrySubentity ,address_line, amount_value))
+                
+                #root = ET.fromstring(xml_data)
+                #print("archivo:", filepath)
+                #print("Ruc emisor:", ruc_emisor)
+                #print("ID de la factura:", invoice_id)
+                #print("Fecha de emisión:", issue_date)
+                #print("Código de tipo de factura:", invoice_type_code)
+                #print("Monto imponible:", taxable_amount)
+                #print("Número de documento", doc_vendedor)
+                #print("Vendedor:",registration_name)
+                #print("Lugar de operacion:", address_line)
+                #print("Total de compras:", amount_value)
+                #print("direccion:", CountrySubentity)
+                #print("------------------------")
 
-            party_legal_entity1 = root.find(".//cac:AccountingSupplierParty/cac:Party/cac:PartyLegalEntity/cac:RegistrationAddress", namespaces=namespace1)
-            if party_legal_entity1 is not None:
-                CountrySubentity = party_legal_entity1.find(".//cbc:Line", namespaces=namespace).text +" "+ party_legal_entity1.find(".//cbc:CountrySubentity", namespaces=namespace).text + "-" + party_legal_entity1.find(".//cbc:CityName", namespaces=namespace).text + "-" + party_legal_entity1.find(".//cbc:District", namespaces=namespace).text
+        # Crear un archivo Excel
 
-            registration_address = root.find(".//cac:DeliveryTerms/cac:DeliveryLocation/cac:Address", namespaces=namespace1)
-            if registration_address is not None:
-                address_line = registration_address.find(".//cbc:Line", namespaces=namespace).text
+        """
+        sadasd
+
         
-            tax_inclusive_amount = root.find(".//cac:LegalMonetaryTotal", namespaces=namespace1)
-            if tax_inclusive_amount is not None:
-                amount_value = tax_inclusive_amount.find(".//cbc:TaxInclusiveAmount", namespaces=namespace).text
+        
+        wb = Workbook()
+        ws = wb.active
+
+        # Agregar encabezados
+        ws.append(["RUC Emisor", "Serie - Número", "Fecha emisión", "Num doc", "Nombre vendedor", "Dirección vendedor", "Lugar operacion", "Importe total"])
+
+        # Agregar datos a la hoja de cálculo
+        for atributo in atributos:
+            ws.append(atributo)
 
 
-            atributos.append((ruc_emisor, invoice_id, issue_date, doc_vendedor, registration_name, CountrySubentity ,address_line, amount_value))
-            
-            #root = ET.fromstring(xml_data)
-            #print("archivo:", filepath)
-            #print("Ruc emisor:", ruc_emisor)
-            #print("ID de la factura:", invoice_id)
-            #print("Fecha de emisión:", issue_date)
-            #print("Código de tipo de factura:", invoice_type_code)
-            #print("Monto imponible:", taxable_amount)
-            #print("Número de documento", doc_vendedor)
-            #print("Vendedor:",registration_name)
-            #print("Lugar de operacion:", address_line)
-            #print("Total de compras:", amount_value)
-            #print("direccion:", CountrySubentity)
-            #print("------------------------")
+        # Especificar el directorio y nombre de archivo
+        directorio = "C:\liquidacion"
+        nombre_archivo = "resultado.xlsx"
+        ruta_completa = os.path.join(directorio, nombre_archivo)
 
-    # Crear un archivo Excel
-    wb = Workbook()
-    ws = wb.active
+        # Guardar el libro de trabajo en el directorio especificado
+        wb.save(ruta_completa)
 
-    # Agregar encabezados
-    ws.append(["RUC Emisor", "Serie - Número", "Fecha emisión", "Num doc", "Nombre vendedor", "Dirección vendedor", "Lugar operacion", "Importe total"])
+        # Guardar el archivo Excel
+        #excel_filename = "atributos.xlsx"
+        #wb.save(excel_filename)
 
-    # Agregar datos a la hoja de cálculo
-    for atributo in atributos:
-        ws.append(atributo)
+        """
+        directorio = "C:\liquidacion"
+        nombre_archivo = "resultado.xlsx"
+        ruta_completa = os.path.join(directorio, nombre_archivo)
 
+        workbook = xlsxwriter.Workbook(ruta_completa)
 
-    # Especificar el directorio y nombre de archivo
-    directorio = "C:\liquidacion"
-    nombre_archivo = "resultado.xlsx"
-    ruta_completa = os.path.join(directorio, nombre_archivo)
+        worksheet = workbook.add_worksheet()
 
-    # Guardar el libro de trabajo en el directorio especificado
-    wb.save(ruta_completa)
+        datos = ["RUC Emisor", "Serie - Número", "Fecha emisión", "Num doc", "Nombre vendedor", "Dirección vendedor", "Lugar operacion", "Importe total"]
 
-    # Guardar el archivo Excel
-    #excel_filename = "atributos.xlsx"
-    #wb.save(excel_filename)
-    print(Fore.GREEN +f"Archivo Excel se ha generado exitosamente.")
+        encabezado_formato = workbook.add_format({'bold': True, 'align': 'center', 'bg_color': '#d9d9d9'})
+
+        for columna, encabezado in enumerate(datos):
+            worksheet.write(0, columna, encabezado, encabezado_formato)
+
+        for fila, fila_datos in enumerate(atributos):
+            for columna, dato in enumerate(fila_datos):
+                worksheet.write(fila+1, columna, dato)
+
+        workbook.close()
+
+        print(Fore.GREEN +f"Archivo Excel se ha generado OK.")
+    
+    except Exception as e:
+        print(Fore.RED + f" Ha ocurrido un error: {e} ")
 
 
 while True:
